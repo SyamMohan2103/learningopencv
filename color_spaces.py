73d0e661e31ca63f06754da34bd2f9cde5aebdb6
Syam Mohan <syammohan2103@gmail.com>
2023-06-07 22:58:45 +0530

    converts image into YUV color space and extracts each information in each layer

--- color_spaces.py
+++ color_spaces.py
@@ -2,6 +2,10 @@ import cv2
 
 # program to conver the input image into different color spaces (GRAY, YUV, HSV)
 img = cv2.imread("D:\projects\learningopencv\scene.jpg")
-converted_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Grayscale image', converted_img)
+converted_img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
+cv2.imshow('Y_channel', converted_img[:,:, 0])
+cv2.imshow('U_channel', converted_img[:,:, 1])
+cv2.imshow('V_channel', converted_img[:,:, 2])
+cv2.imshow('YUV image', converted_img)
 cv2.waitKey()
